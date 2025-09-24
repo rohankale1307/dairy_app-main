4df58b2688d87cf421409f5790aeb65c3ffd1605
2149634 <rohan.skale@tcs.com>
2025-09-24 17:09:39 +0530

    minor changes.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'features/login_user/app_description_screen.dart';
 import 'features/login_user/register_screen.dart';
-import 'features/login_user/welcome_screen.dart';
-import 'features/splash_screens/home_page.dart';
 
 class MyHttpOverrides extends HttpOverrides {
   @override
